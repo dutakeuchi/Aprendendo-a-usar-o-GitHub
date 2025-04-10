ce32f99178a098e179fe92e46ee405769eabc0b3
Eduardo Takeuchi <dutakeuchi@gmail.com>
2025-04-10 11:34:54 -0300

    commit initial

---
+++ teste.py
@@ -0,0 +1,10 @@
+valid = 's'
+
+while valid == 's':
+    valid = input ('Deseja pesquisar a previsão do tempo de outra cidade? S ou N ').lower()
+    if valid == 's':
+        print ('teste')
+    else:
+        print ('Erro')
+
+        
